9ab41ba24542e2c10f3d7cad7ac77b6389c88955
brunoserralheiro <brunoserralheiro@gmail.com>
2017-06-25 04:11:55 +0100

    draw horizontal line left bound and only inside canvas

--- src/main/java/com/sringernature/model/DrawerServiceImpl.java
+++ src/main/java/com/sringernature/model/DrawerServiceImpl.java
@@ -22,7 +22,9 @@ public class DrawerServiceImpl implements DrawerService {
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sringernature.DrawerService#drawCanvas(java.lang.String)
 	 */
 	@Override
@@ -38,7 +40,9 @@ public class DrawerServiceImpl implements DrawerService {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sringernature.DrawerService#drawStraightLine(java.lang.String)
 	 */
 	@Override
@@ -49,35 +53,27 @@ public class DrawerServiceImpl implements DrawerService {
 			x2 = CommandLineHelper.getX2(input);
 			y2 = CommandLineHelper.getY2(input);
 			// showCanvas();
-			if (x1 <= w && x2 < w && y1 <= h && y2 < h 
-					&& x2 < w - x1 & y2 < h - y1) {
-	
-				// Decide if line is horizontal or vertical
-				
-				//line is horizontal
-				if(x2 != x1 && y2 == y1){
-					drawHorizontalLine();
-				//line is vertical
-				}else if(x2 == x1 && y2 != y1){
-									
-					 drawVerticalLine();
-					 
-					 
-					 
-				}else if(x1 == x2 && y1 == y2){
-					System.out.println("Coordinates cannot be the same!");
-				}else
-				{
-					System.out.println("Currently only horizontal or vertical lines are supported.");
-				}
-	
-			}else{
-				System.out.println("Line out of canvas!");
+
+			// Decide if line is horizontal or vertical
+
+			// line is horizontal
+			if (x2 != x1 && y2 == y1) {
+				drawHorizontalLine();
+				// line is vertical
+			} else if (x2 == x1 && y2 != y1) {
+
+				drawVerticalLine();
+
+			} else if (x1 == x2 && y1 == y2) {
+				System.out.println("Coordinates cannot be the same!");
+			} else {
+				System.out.println("Currently only horizontal or vertical lines are supported.");
 			}
+
 		} catch (NumberFormatException e) {
 			// TODO: handle exception
-		} catch (Exception e){
-			
+		} catch (Exception e) {
+
 		}
 	}
 
@@ -85,12 +81,12 @@ public class DrawerServiceImpl implements DrawerService {
 	 * 
 	 */
 	private void drawVerticalLine() {
-		//upwards
-		 if ( y2 < y1){
-			//TODO
-		//downwards	 
-		}else{
-			//TODO
+		// upwards
+		if (y2 < y1) {
+			// TODO
+			// downwards
+		} else {
+			// TODO
 		}
 	}
 
@@ -98,9 +94,22 @@ public class DrawerServiceImpl implements DrawerService {
 	 * 
 	 */
 	private void drawHorizontalLine() {
-		//decide X direction of the line to be drawn
-		if(x2 > x1 ){
+		// decide X direction of the line to be drawn
+		if (x1 < w && x2 < w && y1 < h && y2 < h) {
+			// from left to right
+			if (x2 < x1 ) {
 			
+				int tempX1 = x2;
+				int tempX2 = x1;
+				x2 = tempX2 - 3;
+				x1 = tempX1;
+			}
+			if (x2 < w - x1 & y2 < h - y1) {
+
+				
+			} else {
+				System.out.println("Line out of canvas!");
+			}
 			// redraw x fragment of canvas
 			drawXBorder(w);
 			// start point
@@ -110,26 +119,26 @@ public class DrawerServiceImpl implements DrawerService {
 				System.out.print(" ");
 			}
 			// draws horizontal pixels to end point
-			for (int i = 0; i < x2; i++) {
+			for (int i = 0; i < x2 -2; i++) {
 				System.out.print(pixel);
 			}
 			for (int i = x2 + 1; i < w - 3; i++) {
 				System.out.print(" ");
 			}
 			System.out.println(yLine);
-			drawYBorders(w, h-y2);
+			drawYBorders(w, h - y2);
 			// redraw x fragment of canvas
 			drawXBorder(w);
-		}else if(x2 < x1){
-			//TODO
-		}else if (x2 > x1 && y2 < y1){
-			//TODO
-		}else if (x2 < x1 && y2 > y1){
-			//TODO
+
+		}else{
+			System.out.println("Line out of canvas!");
 		}
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sringernature.DrawerService#showCanvas()
 	 */
 	@Override
@@ -138,6 +147,7 @@ public class DrawerServiceImpl implements DrawerService {
 		drawYBorders(w, h);
 		drawXBorder(w);
 	}
+
 	/**
 	 * 
 	 */
@@ -148,6 +158,7 @@ public class DrawerServiceImpl implements DrawerService {
 		}
 		System.out.println("");
 	}
+
 	@Override
 	public void drawYBorders(int x, int y) {
 		for (int i = 0; i < y; i++) {
@@ -161,7 +172,9 @@ public class DrawerServiceImpl implements DrawerService {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sringernature.DrawerService#drawColorfulCanvas(java.lang.String)
 	 */
 	@Override
@@ -170,7 +183,9 @@ public class DrawerServiceImpl implements DrawerService {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.sringernature.DrawerService#paintCanvas(java.lang.String)
 	 */
 	@Override
